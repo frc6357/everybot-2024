69d37d88045743689819207ff97f76404b352fb4
TheCodeMaker9 <wyatthayden56@gmail.com>
2024-01-20 11:59:37 -0600

    Implemented Dirctional is inverted contants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,5 +15,7 @@ package frc.robot;
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
-  }
+    public static final boolean kLeftMotorIsInverted = false;
+    public static final boolean kRightMotorIsInverted = true;
+  } 
 }
