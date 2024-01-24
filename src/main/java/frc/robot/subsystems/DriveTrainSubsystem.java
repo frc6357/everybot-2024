b1185c9ca958c984f515b38a410030c2cda51ea2
SarahPick <little.s.pickett@gmail.com>
2024-01-23 18:55:47 -0600

    Created differential drive
    also fixed back right CAN ID

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants.DrivetrainConstants;
@@ -19,6 +19,9 @@ public class DriveTrainSubsystem extends SubsystemBase{
     CANSparkMax RightFrontMotor;
     // Instantiate RightBackMotor
     CANSparkMax RightBackMotor;
+    // Instantiate differentialDrive
+    DifferentialDrive differentialDrive;
+    
     // Instantiate the controller
     public DriveTrainSubsystem() {
         super();
@@ -28,13 +31,18 @@ public class DriveTrainSubsystem extends SubsystemBase{
         RightBackMotor = new CANSparkMax(DrivetrainConstants.kRightRearMotorId, MotorType.kBrushless);
 
         LeftFrontMotor.setInverted(true);
-        LeftBackMotor.setInverted(true);
+        LeftBackMotor.follow(LeftFrontMotor);
 
+        RightBackMotor.follow(RightFrontMotor);
+
+        differentialDrive = new DifferentialDrive(LeftFrontMotor,RightFrontMotor);
     }
+    
     // Forward if positive value
     // Backwards is negative value
     // Associate motors with controls
     public void TankDrive(Double LeftSpeed, Double RightSpeed) {
-        LeftFrontMotor.set(LeftSpeed);
+
+        differentialDrive.tankDrive(LeftSpeed,RightSpeed);
     }
 }
