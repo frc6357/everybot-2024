1848dcd669086a88a36246a53a6bbbc7856e06e2
Jonah Rees <156146606+JonahRees@users.noreply.github.com>
2024-02-08 18:23:39 -0600

    I made udjustments to the DriveTrain in order to allow running autos from pathplanner.

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -3,14 +3,26 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
 
 public class DriveTrainSubsystem extends SubsystemBase{
-    
+    // velocity factor is determined by (1/60)*(1/GearReduction)* pi(wheelDiameter)
+    private static final double ENCODER_RPM_TO_MPS = 0.0009433714051;
+    //position factor is determined by (1/GearReduction)* pi(wheelDiameter) or for now velocity factor*60
+    private static final double ENCODER_POS_TO_METERS = ENCODER_RPM_TO_MPS*60;
     // Four motors
     // Instantiate LeftFrontMotor
     CANSparkMax LeftFrontMotor; 
@@ -25,6 +37,12 @@ public class DriveTrainSubsystem extends SubsystemBase{
     
 
     Pigeon2 pigeon;
+    private RelativeEncoder rightFrontEncoder;
+    private RelativeEncoder leftFrontEncoder;
+    private DifferentialDriveOdometry odometry;
+    private SparkPIDController leftPidController;
+    private SparkPIDController rightPidController;
+    private DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Units.inchesToMeters(27.0));
 
     // Instantiate the controller
     public DriveTrainSubsystem() {
@@ -35,12 +53,35 @@ public class DriveTrainSubsystem extends SubsystemBase{
         RightFrontMotor = new CANSparkMax(DrivetrainConstants.kRightFrontMotorId, MotorType.kBrushless);
         RightBackMotor = new CANSparkMax(DrivetrainConstants.kRightRearMotorId, MotorType.kBrushless);
 
+        //Encoder setup
+        leftFrontEncoder = LeftFrontMotor.getEncoder();
+        rightFrontEncoder = RightFrontMotor.getEncoder();
+        leftFrontEncoder.setPositionConversionFactor(ENCODER_POS_TO_METERS);
+        leftFrontEncoder.setVelocityConversionFactor(ENCODER_RPM_TO_MPS);
+        rightFrontEncoder.setPositionConversionFactor(ENCODER_POS_TO_METERS);
+        rightFrontEncoder.setVelocityConversionFactor(ENCODER_RPM_TO_MPS);
+
+        leftFrontEncoder.getPosition();
         LeftFrontMotor.setInverted(true);
         LeftBackMotor.follow(LeftFrontMotor);
 
         RightBackMotor.follow(RightFrontMotor);
-
         differentialDrive = new DifferentialDrive(LeftFrontMotor,RightFrontMotor);
+        
+        //Pid setup
+        leftPidController = LeftFrontMotor.getPIDController();
+        rightPidController = RightFrontMotor.getPIDController();
+        double kP =6e-5;
+        leftPidController.setP(kP);
+        rightPidController.setP(kP);
+        leftPidController.setOutputRange(-1, 1);
+        rightPidController.setOutputRange(-1, 1);
+
+        //odometry
+        odometry = new DifferentialDriveOdometry(pigeon.getRotation2d(), leftFrontEncoder.getPosition(), rightFrontEncoder.getPosition());
+
+        
+
     }
     
     // Forward if positive value
@@ -57,5 +98,22 @@ public class DriveTrainSubsystem extends SubsystemBase{
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Heading", getHeading());
+        odometry.update(pigeon.getRotation2d(), leftFrontEncoder.getPosition(), rightFrontEncoder.getPosition());
     }
+    public Pose2d getPose() {
+        return odometry.getPoseMeters();
+    }
+    public void resetPose(Pose2d pose) {
+        odometry.resetPosition(pigeon.getRotation2d(), leftFrontEncoder.getPosition(), rightFrontEncoder.getPosition(),pose);
+    }
+    public ChassisSpeeds getWheelSpeeds(){
+        DifferentialDriveWheelSpeeds wheelSpeeds =  new DifferentialDriveWheelSpeeds(leftFrontEncoder.getVelocity(),rightFrontEncoder.getVelocity());
+        return kinematics.toChassisSpeeds(wheelSpeeds);
+    }
+    public void driveVelocity(ChassisSpeeds speed){
+        DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(speed);
+        leftPidController.setReference(wheelSpeeds.leftMetersPerSecond, CANSparkMax.ControlType.kVelocity);
+        rightPidController.setReference(wheelSpeeds.rightMetersPerSecond, CANSparkMax.ControlType.kVelocity);
+    }
+
 }
